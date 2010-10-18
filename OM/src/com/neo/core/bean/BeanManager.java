83147c54e8ccf909a8998ff1babac45b1d9670b4
nanlei1987 <nanlei1987@42d2fe55-8468-8079-b84d-68b6966b9a87>
2010-10-18 15:57:21 +0000

    初始导入项目

---
+++ OM/src/com/neo/core/bean/BeanManager.java
@@ -0,0 +1,38 @@
+package com.neo.core.bean;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+/**
+ * Spring控制反转容器Bean管理器
+ * 
+ * @author Nanlei
+ * 
+ */
+public class BeanManager {
+	private static WebApplicationContext wac;
+	private static ServletContext sc;
+
+	public BeanManager() {
+	}
+
+	public static void init(ServletContext context) {
+		sc = context;
+		wac = WebApplicationContextUtils
+				.getRequiredWebApplicationContext(context);
+	}
+
+	public static Object getBean(String beanName) {
+		return wac.getBean(beanName);
+	}
+
+	public static WebApplicationContext getWebApplicationContext() {
+		return wac;
+	}
+
+	public static ServletContext getServletContext() {
+		return sc;
+	}
+}
