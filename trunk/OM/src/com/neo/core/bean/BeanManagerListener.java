acfd8bec0086638b862605e63cf12649fd42abdd
nanlei1987 <nanlei1987@42d2fe55-8468-8079-b84d-68b6966b9a87>
2010-10-18 15:57:21 +0000

    初始导入项目

---
+++ trunk/OM/src/com/neo/core/bean/BeanManagerListener.java
@@ -0,0 +1,19 @@
+package com.neo.core.bean;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Bean管理器所需的Servlet上下文监听器
+ * 
+ * @author Nanlei
+ * 
+ */
+public class BeanManagerListener implements ServletContextListener {
+	public void contextInitialized(ServletContextEvent event) {
+		BeanManager.init(event.getServletContext());
+	}
+
+	public void contextDestroyed(ServletContextEvent event) {
+	}
+}
