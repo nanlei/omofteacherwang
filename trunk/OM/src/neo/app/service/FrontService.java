3f9180901934dec0ded257c3f491a606c244a6ca
nanlei1987 <nanlei1987@42d2fe55-8468-8079-b84d-68b6966b9a87>
2010-10-20 14:39:17 +0000

    添加FrontService

---
+++ trunk/OM/src/neo/app/service/FrontService.java
@@ -0,0 +1,5 @@
+package neo.app.service;
+
+public class FrontService extends BaseService {
+
+}
--- trunk/OM/src/neo/app/service/ServiceManager.java
+++ trunk/OM/src/neo/app/service/ServiceManager.java
@@ -2,6 +2,9 @@ package neo.app.service;
 
 public class ServiceManager {
 	private UserService userService;
+	
+	
+	private FrontService frontService;
 
 	public UserService getUserService() {
 		return userService;
@@ -10,5 +13,15 @@ public class ServiceManager {
 	public void setUserService(UserService userService) {
 		this.userService = userService;
 	}
+
+	public FrontService getFrontService() {
+		return frontService;
+	}
+
+	public void setFrontService(FrontService frontService) {
+		this.frontService = frontService;
+	}
+	
+	
 
 }
