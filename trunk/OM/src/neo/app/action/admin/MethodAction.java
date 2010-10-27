5be8c343ab63358335c806e3a348ec7b0bd8ba86
nanlei1987 <nanlei1987@42d2fe55-8468-8079-b84d-68b6966b9a87>
2010-10-27 08:40:25 +0000

    奥数方法功能完成

--- trunk/OM/src/neo/app/action/admin/MethodAction.java
+++ trunk/OM/src/neo/app/action/admin/MethodAction.java
@@ -1,13 +1,25 @@
 package neo.app.action.admin;
 
+import java.util.Map;
+
 import neo.app.action.BaseAction;
 import neo.core.common.PagingList;
 
 public class MethodAction extends BaseAction {
 	private PagingList methodList;
+	private Map methodMap;
+	private String id;
 
 	public PagingList getMethodList() {
 		return methodList;
+	}
+
+	public Map getMethodMap() {
+		return methodMap;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	/**
@@ -35,6 +47,53 @@ public class MethodAction extends BaseAction {
 		} catch (Exception e) {
 			setResult(ERROR);
 			addMessage("奥数方法添加失败");
+			addRedirURL("返回", "@back");
+		}
+		return EXECUTE_RESULT;
+	}
+
+	/**
+	 * 获取一条方法信息供修改
+	 * 
+	 * @return
+	 */
+	public String preUpdate() {
+		methodMap = getServMgr().getMethodService().getMethodById(id);
+		return "preUpdate";
+	}
+
+	/**
+	 * 更新奥数方法
+	 * 
+	 * @return
+	 */
+	public String update() {
+		try {
+			getServMgr().getMethodService().updateMethodById(getParameters(),
+					id);
+			addMessage("奥数方法更新成功");
+			addRedirURL("返回", "list.action");
+		} catch (Exception e) {
+			setResult(ERROR);
+			addMessage("奥数方法更新失败");
+			addRedirURL("返回", "@back");
+		}
+		return EXECUTE_RESULT;
+	}
+
+	/**
+	 * 删除奥数方法
+	 * 
+	 * @return
+	 */
+	public String delete() {
+		try {
+			getServMgr().getMethodService().deleteMethodById(id);
+			addMessage("奥数方法删除成功");
+			addRedirURL("返回", "list.action");
+		} catch (Exception e) {
+			setResult(ERROR);
+			addMessage("奥数方法删除失败");
 			addRedirURL("返回", "@back");
 		}
 		return EXECUTE_RESULT;
