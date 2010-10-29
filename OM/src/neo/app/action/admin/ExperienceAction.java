2a3a103a1b6a0cd3e1a42c8424a110a0684deece
nanlei1987 <nanlei1987@42d2fe55-8468-8079-b84d-68b6966b9a87>
2010-10-29 14:32:22 +0000

    奥数心得-分类管理全部完成

--- OM/src/neo/app/action/admin/ExperienceAction.java
+++ OM/src/neo/app/action/admin/ExperienceAction.java
@@ -1,14 +1,26 @@
 package neo.app.action.admin;
 
+import java.util.Map;
+
 import neo.app.action.BaseAction;
 import neo.core.common.PagingList;
 
 public class ExperienceAction extends BaseAction {
 	// 分类相关
 	private PagingList categoryList;
+	private Map categoryMap;
+	private String categoryId;
 
 	public PagingList getCategoryList() {
 		return categoryList;
+	}
+
+	public Map getCategoryMap() {
+		return categoryMap;
+	}
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
 	}
 
 	/**
@@ -36,6 +48,54 @@ public class ExperienceAction extends BaseAction {
 		} catch (Exception e) {
 			setResult(ERROR);
 			addMessage("分类添加失败");
+			addRedirURL("返回", "@back");
+		}
+		return EXECUTE_RESULT;
+	}
+
+	/**
+	 * 获取分类详情供修改
+	 * 
+	 * @return
+	 */
+	public String preUpdateCategory() {
+		categoryMap = getServMgr().getExperienceService().getCategoryById(
+				categoryId);
+		return "preUpdate";
+	}
+
+	/**
+	 * 更新分类详情
+	 * 
+	 * @return
+	 */
+	public String updateCategory() {
+		try {
+			getServMgr().getExperienceService().updateCategoryById(
+					getParameters(), getIP(), categoryId);
+			addMessage("分类修改成功");
+			addRedirURL("返回", "list.action");
+		} catch (Exception e) {
+			setResult(ERROR);
+			addMessage("分类修改失败");
+			addRedirURL("返回", "@back");
+		}
+		return EXECUTE_RESULT;
+	}
+
+	/**
+	 * 删除分类
+	 * 
+	 * @return
+	 */
+	public String deleteCategory() {
+		try {
+			getServMgr().getExperienceService().deleteCategoryById(categoryId);
+			addMessage("分类删除成功");
+			addRedirURL("返回", "list.action");
+		} catch (Exception e) {
+			setResult(ERROR);
+			addMessage("分类删除失败");
 			addRedirURL("返回", "@back");
 		}
 		return EXECUTE_RESULT;
--- OM/src/neo/app/service/ExperienceService.java
+++ OM/src/neo/app/service/ExperienceService.java
@@ -32,4 +32,47 @@ public class ExperienceService extends BaseService {
 				"name,description");
 		jt.update(SQL_ADD_CATEGORY, params[0], params[1], ip);
 	}
+
+	// 根据ID查找教育心得分类
+	private static final String SQL_GET_CATEGORY_BY_ID = "select * from om_experience_division where ID=?";
+
+	/**
+	 * 根据ID查找教育心得分类
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public Map getCategoryById(String categoryId) {
+		return jt.queryForMap(SQL_GET_CATEGORY_BY_ID, categoryId);
+	}
+
+	// 更新教育心得分类
+	private static final String SQL_UPDATE_CATEGORY_BY_ID = "update om_experience_division set NAME=?,DESCRIPTION=?,UPDATETIME=now(),UPDATEIP=? where ID=?";
+
+	/**
+	 * 更新教育心得分类
+	 * 
+	 * @param parameterMap
+	 * @param ip
+	 * @param categoryId
+	 */
+	public void updateCategoryById(Map parameterMap, String ip,
+			String categoryId) {
+		Object[] params = MapUtil.getObjectArrayFromMap(parameterMap,
+				"name,description");
+		jt.update(SQL_UPDATE_CATEGORY_BY_ID, params[0], params[1], ip,
+				categoryId);
+	}
+
+	// 删除分类
+	private static final String SQL_DELETE_CATEGORY_BY_ID = "delete from om_experience_division where ID=?";
+
+	/**
+	 * 删除分类
+	 * 
+	 * @param categoryId
+	 */
+	public void deleteCategoryById(String categoryId) {
+		jt.update(SQL_DELETE_CATEGORY_BY_ID, categoryId);
+	}
 }
